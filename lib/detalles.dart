0c2ad38f4eb224219d3fe8f994b0c485296e4eb5
lupitaesp <51250754+lupitaesp@users.noreply.github.com>
2020-06-19 21:55:34 -0500

    Vista de detalles

--- lib/detalles.dart
+++ lib/detalles.dart
@@ -2,13 +2,50 @@ import 'students.dart';
 import 'package:flutter/material.dart';
 import 'convertidor.dart';
 import 'package:image_picker/image_picker.dart';
+import 'students.dart';
+import 'crud_operations.dart';
+
 class DetailPage extends StatelessWidget{
 
   final Student student;
   DetailPage(this.student);
-
+  int opcion;
+  final formkey = new GlobalKey<FormState>();
+  String photoName;
+  int currentUserId;
+  var bdHelper;
   String imagen;
 
+    String name;
+  String surname;
+  String ap;
+  String mat;
+  String mail;
+  String num;
+  String valor;
+
+/*
+void updateData(){
+    print("Valor de Opción");
+    print(opcion);
+    if (formkey.currentState.validate()) {
+      formkey.currentState.save();
+      //NOMBRE
+      if (opcion==1) {
+        Student stu = Student(currentUserId,valor, surname, ap, mat, mail, num, photoName);
+        bdHelper.update(stu);
+      }
+      //APELLIDO PATERNO
+      else if (opcion==2) {
+        Student stu = Student(currentUserId,name, valor, ap, mat, mail, num, photoName);
+        bdHelper.update(stu);
+      }
+      cleanData();
+      refreshList();
+    }
+  }
+*/
+/*
 //Metodo para imagen
   pickImagefromGallery(BuildContext context) {
     ImagePicker.pickImage(source: ImageSource.gallery).then((imgFile) {
@@ -23,6 +60,7 @@ class DetailPage extends StatelessWidget{
     });
   }
 
+
   pickImagefromCamera(BuildContext context) {
     ImagePicker.pickImage(source: ImageSource.camera).then((imgFile) {
       String imgString = Convertir.base64String(imgFile.readAsBytesSync());
@@ -30,7 +68,8 @@ class DetailPage extends StatelessWidget{
       //bdHelper.insert(photo);
       //fotos();
       imagen = imgString;
-      Navigator.of(context).pop();
+
+    //  Navigator.of(context).pop(bdHelper.update(stu));
       return imagen;
     });
   }
@@ -62,15 +101,17 @@ class DetailPage extends StatelessWidget{
           ));
         });
   }
+  */
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        backgroundColor: Colors.white,
+    backgroundColor: Colors.deepPurple[300],
         appBar: AppBar(
-          title: Text(student.name.toString().toUpperCase()),
+          title: Text(student.name.toString().toUpperCase()+" "+student.surname.toString().toUpperCase()),
           centerTitle: true,
-          backgroundColor: Colors.blueGrey,
+          backgroundColor: Colors.deepPurple,
+          automaticallyImplyLeading: false,
         ),
         body: Stack(
           children: <Widget>[
@@ -91,7 +132,7 @@ class DetailPage extends StatelessWidget{
               child: Container(
                 child: SingleChildScrollView(
                   child: Card(
-                    color: Colors.transparent,
+                    color: Colors.white,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0)),
                     child: Column(
@@ -104,16 +145,18 @@ class DetailPage extends StatelessWidget{
                         CircleAvatar(
                           minRadius: 80.0,
                               maxRadius:  80.0,
-                              backgroundColor: Colors.white,
+                              backgroundColor: Colors.black,
                               backgroundImage: Convertir.imageFromBase64sString(student.photoName).image,
-                    
+                           
+                              
                         ),
-                        new RaisedButton( color: Colors.deepPurple[200],
+                       /* new RaisedButton( color: Colors.deepPurple[200],
                           onPressed: () {
                             _selectfoto(context);
                           },
-                          child: Text("Update image", textAlign: TextAlign.center,),),
+                          child: Text("Update image", textAlign: TextAlign.center,),),*/
                         new Padding(padding: EdgeInsets.all(10.0),),
+              
                         new Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
@@ -121,63 +164,69 @@ class DetailPage extends StatelessWidget{
                         Text(
                           student.name.toString().toUpperCase(),
                           style: TextStyle(
-                              fontSize: 25.0,
-                              //fontWeight: FontWeight.bold
+                              fontSize: 30.0,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
                           ),
                         ),
                         //APELLIDO PATERNO
-                        Text(
-                          student.surname.toString().toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              //fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        //APELLIDO MATERNO
-                        Text(
-                          student.ap.toString().toUpperCase(),
-                          style: TextStyle(
-                              fontSize: 25.0,
-                              //fontWeight: FontWeight.bold
-                          ),
-                        ),
                           ],
                         ),
-                        //MATRICULA
-                        new Padding(padding: EdgeInsets.all(10.0),),
-                        Text("Matricula: ${student.mat}",
-                          style: TextStyle(
-                              fontSize: 20.0,
-                              color: Colors.black45
-                          ),
-                        ),
-                        //EMAIL
-                        new Padding(padding: EdgeInsets.all(15.0),),
+                        new Padding(padding: EdgeInsets.all(6.0),),
                         new Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: <Widget>[
-                            Text("Email: ${student.mail}",
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                      ],
-                    ),
+                        //NOMBRE
+                        Text(
+                          student.surname.toString().toUpperCase()
+                          +" "+ student.ap.toString().toUpperCase(),
+                          style: TextStyle(
+                              fontSize: 20.0,
+                               color: Colors.black,
+                             // fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        //APELLIDO PATERNO
+                          ],
+                        ),
+                        Divider(
+                          color: Colors.deepPurpleAccent,
+                           indent: 20,
+                              endIndent: 20,
+                              thickness: 3.0
+                        ),
+
+                        //MATRICULA
+                        new Padding(padding: EdgeInsets.all(10.0),
+                        ),
+                      RaisedButton.icon(
+                        onPressed: (){ print('Button Clicked.'); },
+                        shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                        label: Text("Matricula: "+ "     "+ "${student.mat}", 
+                              style: TextStyle(color: Colors.white),),
+                        icon: Icon(Icons.info, color:Colors.white,), 
+                        color: Colors.deepPurple[300],),
+                                            
+                        //EMAIL
+                        new Padding(padding: EdgeInsets.all(10.0),),
+                        RaisedButton.icon(
+                        onPressed: (){ print('Button Clicked.'); },
+                        shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                        label: Text("Email: "+ "     "+ "${student.mail}", 
+                              style: TextStyle(color: Colors.white),),
+                        icon: Icon(Icons.mail, color:Colors.white,), 
+                        color: Colors.deepPurple[400],),
                     new Padding(padding: EdgeInsets.all(10.0),),
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Text("Telefono: ${student.num}",
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold
-                              ),
-                            ),
-                        ],
-                      ),
+                   RaisedButton.icon(
+                        onPressed: (){ print('Button Clicked.'); },
+                        shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                        label: Text("Telefono: "+ "     "+ "${student.num}", 
+                              style: TextStyle(color: Colors.white),),
+                        icon: Icon(Icons.phone, color:Colors.white,), 
+                        color: Colors.deepPurple[500],),
                     ],
                   ),
                 ),
